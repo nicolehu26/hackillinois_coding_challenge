3962d7a316f797ef1b2b4d711e8d84a58ef515fe
nicolehu26 <nicolehu2626@gmail.com>
2024-09-15 13:02:20 -0500

    Initial Commit

---
+++ IOS HackIllinois/IOS_HackIllinoisApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_HackIllinoisApp.swift
+//  IOS HackIllinois
+//
+//  Created by Nicole Hu on 9/15/24.
+//
+
+import SwiftUI
+
+@main
+struct IOS_HackIllinoisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
